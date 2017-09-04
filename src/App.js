e3bba4c173da4e2c6f099060e6bc1072dea16e0f
Lionel Rowe <lionel.rowe@gmail.com>
2017-09-04 21:42:54 +0800

    Add replay screen and player select

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import './App.css';
 
 function Square(props) {
   
+  // TODO: Add logic to check if the current square is one
+  // of the winning ones; if it is, add class `.winning`
+  // (styled in the CSS for win-screen board).
+
   return (
     <div className={"Square " + props.borders} onClick={props.onClick}>{props.val}</div>
-  )
+  );
 }
 
-class PlayArea extends React.Component {
-  
+class Board extends React.Component {
+
   constructor() {
     super();
-    this.state = {
-      squares: Array(9).fill(null),
-      itsXsTurn: true,
-    };
     this.borders = [
     'right bottom',
     'right bottom',
@@ -28,32 +28,14 @@ class PlayArea extends React.Component {
     '',
     ];
   }
-  
+
   renderSquare(seq) {
     return (
-      <Square seq={seq} val={this.state.squares[seq]} borders={this.borders[seq]} onClick={() => this.handleClick(seq)} />
+      <Square seq={seq} val={this.props.squares[seq]} borders={this.borders[seq]} onClick={this.props.onClick ? (() => this.props.onClick(seq)) : null} />
     )
   }
-  
-  handleClick(seq) {
 
-    if (this.state.squares[seq] || this.state.winStatus) {
-      return;
-    }
-    
-    const squares = this.state.squares.slice();
-    squares[seq] = (this.state.itsXsTurn ? 'X' : 'O');
-
-    this.setState(
-      {
-        squares: squares,
-        itsXsTurn: !this.state.itsXsTurn,
-        winStatus: checkWinner(squares),
-      }
-    );
-  }
-
-  renderBoard() {
+  render() {
     return (
       <div className="board">
         <div className="row">
@@ -74,31 +56,81 @@ class PlayArea extends React.Component {
       </div>
     )
   }
+}
+
+class PlayArea extends React.Component {
+
+  constructor() {
+    super();
+    this.state = this.initState();
+    this.state.players = 1;
+  }
+
+  initState() {
+    const initialState = {
+      squares: Array(9).fill(null)
+      , itsXsTurn: true
+      , winStatus: null
+    };
+    return initialState;
+  }
+
+  handleClick(seq) {
+    if (this.state.squares[seq] || this.state.winStatus) {
+      return;
+    }
+
+    console.log(this);
+    
+    const squares = this.state.squares.slice();
+    squares[seq] = (this.state.itsXsTurn ? 'X' : 'O');
+
+    this.setState(
+      {
+        squares: squares,
+        itsXsTurn: !this.state.itsXsTurn,
+        winStatus: checkWinner(squares),
+      }
+    );
+
+  }
 
   renderPlayAgainScreen() {
     if (this.state.winStatus) {
       return (
-        <PlayAgainScreen winner={this.state.winStatus} />
+        <PlayAgainScreen winner={this.state.winStatus} squares={this.state.squares} clickHandler={() => this.reset()} />
       )
-    } 
+    }
 
   }
 
   render() {
-    
     return (
       <div className="PlayArea">
-        {this.renderBoard()}
+        <Board itsXsTurn={this.state.itsXsTurn} onClick={(seq) => this.handleClick(seq)} squares={this.state.squares} />
         <div className="statusLine">
-          Current turn: {this.state.itsXsTurn ? 'X' : 'O'}
+          {this.state.winStatus ? '' : `Current turn: ${this.state.itsXsTurn ? 'X' : 'O'}`}
         </div>
-
+        <Controls players={this.state.players} clickHandler={(value) => this.setState({players: value})} />
         {this.renderPlayAgainScreen()}
-
       </div>
-
     );
   }
+
+  reset() {
+    this.setState(this.initState());
+  }
+
+}
+
+function Controls(props) {
+  return (
+    <form>
+      <label><input type="radio" name="players" value="one" checked={props.players === 1} onClick={() => props.clickHandler(1)} /><span />One player</label>
+      <br />
+      <label><input type="radio" name="players" value="two" checked={props.players === 2} onClick={() => props.clickHandler(2)} /><span />Two players</label>
+    </form>
+  );
 }
 
 class PlayAgainScreen extends React.Component {
@@ -107,14 +139,20 @@ class PlayAgainScreen extends React.Component {
     return (
       <div className="PlayAgainScreen">
         <div className="container">
-          <h2>{this.props.winner === 'draw' ? `It\'s a tie!` : `${this.props.winner} wins!`}</h2>
+          <h2>{this.props.winner === 'draw' ? `It's a tie!` : `${this.props.winner} wins!`}</h2>
+          <Board squares={this.props.squares} />
           <div className="optionButtons">
-            <input type= "button" value="Play again?" />
+            <input type= "button" value="Play again?" onClick={this.props.clickHandler} />
           </div>
         </div>
       </div>
     );
   }
+
+  playAgain() {
+    PlayArea.reset();
+  }
+
 }
 
 class App extends React.Component {
@@ -130,17 +168,17 @@ class App extends React.Component {
 
 function checkWinner(sqArr) {
   const wins = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6],
+  [0,1,2]
+  , [3,4,5]
+  , [6,7,8]
+  , [0,3,6]
+  , [1,4,7]
+  , [2,5,8]
+  , [0,4,8]
+  , [2,4,6]
   ]
 
-  //ok so this is ugly af I admit...
+  //ok so this is ugly, I admit...
   for (let i=0;i<wins.length;i++) {
     if (sqArr[wins[i][0]] && sqArr[wins[i][0]] === sqArr[wins[i][1]] && sqArr[wins[i][0]] === sqArr[wins[i][2]]) {
       return sqArr[wins[i][0]];
